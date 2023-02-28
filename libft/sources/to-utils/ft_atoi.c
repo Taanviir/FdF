6e0e81a4230a2965999b26d16f7d3d8c21f98958
Tanvir Ahmed Anas <tanas@lab1r4s12.42abudhabi.ae>
2023-02-28 16:12:25 +0400

    done

--- libft/sources/to-utils/ft_atoi.c
+++ libft/sources/to-utils/ft_atoi.c
@@ -6,11 +6,11 @@
 /*   By: tanas <tanas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/06 09:23:32 by tanas             #+#    #+#             */
-/*   Updated: 2023/02/24 20:20:32 by tanas            ###   ########.fr       */
+/*   Updated: 2023/02/28 16:03:38 by tanas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "libft.h"
+#include "libft.h"
 
 int	ft_atoi(const char *str)
 {
